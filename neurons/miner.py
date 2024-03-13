cd1900263476c4208864596c658a1eb034b4553e
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-03-13 23:59:23 +0500

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -180,7 +180,7 @@ def main(config):
             bt.logging.info(f"Checking the path of the model supplies: {config.bark_vc_path}")
             bt.logging.info(f"Using the Voice Clone with the supplied model: {config.clone_model}")
             bark_vc_model = config.bark_vc_path if config.bark_vc_path else None
-            voice_clone_model = ModelLoader(model_dir=bark_vc_model, gpu_id=device)
+            voice_clone_model = ModelLoader(model_dir=bark_vc_model) # , gpu_id=device
         elif config.clone_model is not None and config.clone_model == "elevenlabs/eleven" and config.eleven_api is not None:
             bt.logging.info(f"Using the Voice Clone with the supplied model: {config.clone_model}")
             voice_clone_model = ElevenLabsClone(config.eleven_api)

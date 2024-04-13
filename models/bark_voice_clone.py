f47fef997f18f0cdfa09e72db6a710066b0c5e55
isharab <sherijigarx@gmail.com>
2024-04-13 18:30:21 +0500

    done

--- models/bark_voice_clone.py
+++ models/bark_voice_clone.py
@@ -1582,9 +1582,10 @@ def codec_decode(fine_tokens):
 
 
 class ModelLoader:
-   def __init__(self, model_dir=None):
+   def __init__(self, model_dir=None, gpu_id=0):
         self.model_dir = model_dir
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = torch.device(f"cuda:{gpu_id}" if torch.cuda.is_available() else "cpu")  # Updated to use specific GPU
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)  # Ensure that only the specific GPU is visible to this script
         self.model = self.load_codec_model()
         self.hubert_manager = self.load_hubert_manager()
         self.hubert_model = self.load_hubert_model()
